f09f7cb5a2f06266400ae427a440fbea431f3394
rafaelbarbosas <rafael.1940.b@gmail.com>
2022-09-16 19:07:50 -0300

    Add functions to create UART and control to the analogic display

--- main.c
+++ main.c
@@ -10,15 +10,53 @@
 #define TRUE 1
 #define QTD 4 //Nr de amostras por canal
 
+#define MCU_CLOCK                   1048576
+#define HALF_MCU_CLOCK              529288
+#define A_CLOCK                     32768
+
+#define PWM_SERVO_FREQ              50     // 50Hz
+#define PWM_SERVO_PERIOD            (MCU_CLOCK / PWM_SERVO_FREQ)
+
+// PWM timer values to servo with
+//  max position of servo in degrees = 180 degrees
+//  minimum servo duty cicle - 0.5ms
+//  maximum servo duty cicle - 2.5ms
+//  using the MCU_CLOCK with 6th output mode
+
+unsigned int degree_to_period[] = { 2621, 2609, 2598, 2586, 2574, 2563, 2551, 2539, 2528, 2516,
+                                    2504, 2493, 2481, 2470, 2458, 2446, 2435, 2423, 2411, 2400,
+                                    2388, 2376, 2365, 2353, 2341, 2330, 2318, 2306, 2295, 2283,
+                                    2272, 2260, 2248, 2237, 2225, 2213, 2202, 2190, 2178, 2167,
+                                    2155, 2143, 2132, 2120, 2108, 2097, 2085, 2073, 2062, 2050,
+                                    2038, 2027, 2015, 2004, 1992, 1980, 1969, 1957, 1945, 1934,
+                                    1922, 1910, 1899, 1887, 1875, 1864, 1852, 1840, 1829, 1817,
+                                    1806, 1794, 1782, 1771, 1759, 1747, 1736, 1724, 1712, 1701,
+                                    1689, 1677, 1666, 1654, 1642, 1631, 1619, 1607, 1596, 1584,
+                                    1572, 1561, 1549, 1538, 1526, 1514, 1503, 1491, 1479, 1468,
+                                    1456, 1444, 1433, 1421, 1409, 1398, 1386, 1374, 1363, 1351,
+                                    1340, 1328, 1316, 1305, 1293, 1281, 1270, 1258, 1246, 1235,
+                                    1223, 1211, 1200, 1188, 1176, 1165, 1153, 1141, 1130, 1118,
+                                    1106, 1095, 1083, 1072, 1060, 1048, 1037, 1025, 1013, 1002,
+                                    990, 978, 967, 955, 943, 932, 920, 908, 897, 885,
+                                    874, 862, 850, 839, 827, 815, 804, 792, 780, 769,
+                                    757, 745, 734, 722, 710, 699, 687, 675, 664, 652,
+                                    640, 629, 617, 606, 594, 582, 571, 559, 547, 536,
+                                    524};
+
+
 void ADC_config(void);
 void USCI_A0_config(void);
 void TA0_config(void);
 void GPIO_config(void);
+void servo_config(void);
 
 void write_base_lcd(char address);
 void write_joyst_lcd(char address, int avg_int, float avg_flt,
                      float max, float min);
 void write_base_uart(void);
+void write_joyst_uart(char address, int avg_int, float avg_flt,
+                      float max, float min);
+void write_servo(float measure);
 
 // Variaveis globais
 volatile int vrx[QTD];  //Vetor x
@@ -47,6 +85,7 @@ int main(void)
     TA0_config();
     ADC_config();
     USCI_A0_config();
+    servo_config();
 
     char address = setup_lcd();
     write_base_lcd(address);
@@ -84,7 +123,10 @@ int main(void)
             write_joyst_lcd(address, media_x, volt_x,
                                 max_x, min_x);
             // atualizar UART  com o canal A1
+            write_joyst_uart(address, media_x, volt_x,
+                             max_x, min_x);
             // atualizar servo com o canal A1
+            write_servo(volt_x);
         }
 
         if(chanel==2){
@@ -92,7 +134,10 @@ int main(void)
             write_joyst_lcd(address, media_y, volt_y,
                                 max_y, min_y);
             // atualizar UART  com o canal A2
+            write_joyst_uart(address, media_y, volt_y,
+                             max_y, min_y);
             // atualizar servo com o canal A2
+            write_servo(volt_y);
         }
 
         if((P6IN&BIT3) == 0){
@@ -153,15 +198,26 @@ void write_joyst_lcd(char address, int avg_int, float avg_flt,
 }
 
 void write_base_uart(void){
-    char ch, letra_rx;
-    while(TRUE){
-        for(ch = 'A'; ch <= 'Z'; ch++){
-            while ( (UCA0IFG&UCTXIFG)==0); //Esperar TXIFG=1
-            UCA0TXBUF=ch;
-            while ( (UCA0IFG&UCRXIFG)==0); //Esperar RXIFG=1
-            letra_rx=UCA0RXBUF;
-        }
-    }
+    // TO-DO
+//    volatile char ch, letra_rx;
+//    while(TRUE){
+//        for(ch = 'A'; ch <= 'Z'; ch++){
+//            while ( (UCA0IFG&UCTXIFG)==0); //Esperar TXIFG=1
+//            UCA0TXBUF=ch;
+//            while ( (UCA0IFG&UCRXIFG)==0); //Esperar RXIFG=1
+//            letra_rx=UCA0RXBUF;
+//        }
+//    }
+}
+
+void write_joyst_uart(char address, int avg_int, float avg_flt,
+                      float max, float min){
+    // TO - DO
+}
+
+void write_servo(float measure){
+    int servo_position = (measure*10*180)/50;
+    TA2CCR2 = degree_to_period[servo_position];
 }
 
 void ADC_config(void)
@@ -197,6 +253,16 @@ void TA0_config(void)
     TA0CCTL1 = OUTMOD_6; //Out = modo 6
     TA0CCR0 = 32767 / 32; //32 Hz (16 Hz por canal)
     TA0CCR1 = TA0CCR0 / 2; //Carga 50%
+}
+
+void servo_config(void){
+    // Timer A2.2 -> moves servo
+    TA2CTL = TASSEL_2 | MC_1;           // SMCLK, upmode
+    TA2CCTL2 = OUTMOD_6;                // TA2CCR2 toggle/reset
+    TA2CCR0 = PWM_SERVO_PERIOD - 1;     // PWM Period - 50Hz
+    TA2CCR2 = 0;                        // TA2CCR2 PWM Duty Cycle
+    P2DIR |= BIT5;                      // P2.5 = output
+    P2SEL |= BIT5;                      // P2.5 = TA2 output
 }
 
 void GPIO_config(void)
